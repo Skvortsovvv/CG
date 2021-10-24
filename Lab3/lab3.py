ce588cd65f6120b6d15b7d75e9972b26667c08b0
Vladimir <vladimirs001@yandex.ru>
2021-10-24 23:50:30 +0300

    done?

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -3,11 +3,12 @@ import numpy as np
 from skimage import io
 
 img = io.imread(r".\\corgi.jpg")
-img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(float) # Делаем одноканальное изображение. Применяем 
+cv2.imshow('doge',img1)
+cv2.waitKey(0)
 
 edge_x = cv2.Sobel(img1,cv2.CV_64F,1,0,ksize=3)
 edge_y = cv2.Sobel(img1,cv2.CV_64F,0,1,ksize=3)    
-edge = np.sqrt(edge_x**2 + edge_y**2)    # image can be normalized to 
-                                         # fit into 0..255 color space
+edge = np.sqrt(edge_x**2 + edge_y**2)    
 
 cv2.imwrite("out.jpg", edge)
